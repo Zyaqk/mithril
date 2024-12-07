d94830834c07e41970ca0fae7bf32f03ca32a695
Zyaqk <ukaminskiy06@gmail.com>
2024-12-07 21:22:53 +0100

    v5.2.2

--- static/scripts/payment.js
+++ static/scripts/payment.js
@@ -312,7 +312,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div id="message">
                         <div class="container">
                             <p>${data.response.message}</p>
-                            <button id="messagebtn">${data.response.buttonCaption}</button>
                         </div>
                     </div>
             `;
